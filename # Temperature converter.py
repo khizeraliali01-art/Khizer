da9e6de20e12426571e825add87b771cd400459d
khizeraliali01-art <khizeraliali01@gmail.com>
2026-07-19 07:36:52 -0700

    Add files via upload

---
+++ # Temperature converter.py
@@ -0,0 +1,10 @@
+# Temperature converter
+
+# User sa fahrenheit ki value input lo.
+fahrenheit = float (input("Enter fahrenheit :")) 
+
+# fahrenheit ko 32 se multiply kar ke celsius mein convert kar raha hai.
+celsius = fahrenheit * 32 
+
+# Result screen par print karta ha
+print ("total  celsius : " , celsius )
